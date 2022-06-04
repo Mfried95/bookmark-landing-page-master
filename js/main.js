13ca286c6ca754a0a6666ea397e14bf940acd520
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-04 14:30:40 -0400

    Updated nav

--- js/main.js
+++ js/main.js
@@ -2,18 +2,24 @@
 
 
 // OPEN MENU 
-const hamburgerOpen = document.getElementById("hamburger")
+const hamburgerOpen = document.getElementById("hamburger-open")
 const dropOpen = document.querySelector(".mobile-nav")
 
 hamburgerOpen.addEventListener("click", function(){
    dropOpen.style.display = "block"
+   dropClose.style.display = "none"
 })
 
-// CLOSE MENU
 
+
+
+
+// CLOSE MENU
 const hamburgerClose = document.getElementById("hamburger-close")
-const dropClose = document.querySelector(".mobile-nav active")
+const dropClose = document.querySelector(".mobile-nav-open")
 
 hamburgerClose.addEventListener("click", function(){
     dropOpen.style.display = "none"
+    dropClose.style.display = "block"
+    dropClose.style.display = "flex"
 })
